f0786bda9725002078ba307868633f5efaa3aff1
Twelvee <redapple54r@gmail.com>
2018-04-19 22:41:41 +0700

    Rotate and folder controoler reloader

--- Assets/Scripts/Floor/FloorController.cs
+++ Assets/Scripts/Floor/FloorController.cs
@@ -11,46 +11,62 @@ public class FloorController : MonoBehaviour {
 	public int sizeLeft;
 	public int sizeRight;
 	public int sizeBlock;
-	public static bool allowFloorClick = false;
+	public static bool allowFloorClick = true;
 	private GridController grid;
 	public static GameObject curObject = null;
 	public static GameObject prevObject = null;
 	public static string prevGrid = "";
 	// Use this for initialization
 	void Start () {
+		GuiManager.Instance.PropMenu.SetActive (false);
 		grid = new GridController ();
 		grid.createGrid (sizeLeft,sizeRight,sizeBlock);
 		grid.spawnFloor (floorTexture);
 		grid.spawnWalls (wallTexture);
+		spawnDefault ();
+	}
+
+	void spawnDefault(){
+		PropController.create (StaticProps.Instance.modular_kitchen_table_1, "12", new Vector3(-90,0,-90), 0.5F);
+		PropController.create (StaticProps.Instance.modular_kitchen_table_1, "13", new Vector3(-90,0,-90), 0.5F);
+		PropController.create (StaticProps.Instance.cash_register, "13", new Vector3(-90,0,0), 8.5F);
 	}
 
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButtonDown(0)) {
-			
-			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-			RaycastHit hit;
-			if (Physics.Raycast(ray, out hit))
-			{
-				if(prevObject!=null)
-					prevObject.GetComponent<Renderer> ().material.color = Color.white;
-				if (hit.collider.gameObject.name.StartsWith ("floor_")) {
-					string key = hit.collider.gameObject.name.Replace ("floor_", "");
-					curObject = GridController.floor[key];
-					prevGrid = key;
-					Debug.Log (key);
-					if(curObject==null)
-						hit.collider.gameObject.GetComponent<Renderer> ().material.color = Color.green;
-					else
-						hit.collider.gameObject.GetComponent<Renderer> ().material.color = Color.red;
-				}
-				if (hit.collider.gameObject.name.StartsWith ("wall_")) {
-					string key = hit.collider.gameObject.name.Replace ("wall_", "");
-					curObject = GridController.walls[key];
-					prevGrid = key;
-				}
-				prevObject = hit.collider.gameObject;
+			if (allowFloorClick) {
+				Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+				RaycastHit hit;
+				if (Physics.Raycast(ray, out hit))
+				{
+					if(prevObject!=null)
+						prevObject.GetComponent<Renderer> ().material.color = Color.white;
+					if (hit.collider.gameObject.name.StartsWith ("floor_")) {
+						string key = hit.collider.gameObject.name.Replace ("floor_", "");
+						curObject = GridController.floor[key];
+						prevGrid = key;
+						Debug.Log (key);
+						if (curObject == null) {
+							hit.collider.gameObject.GetComponent<Renderer> ().material.color = Color.green;
+							GuiManager.Instance.setGrid (key);
+							allowFloorClick = false;
+							GuiManager.Instance.PropMenu.SetActive (true);
+							GuiManager.rotateButtonActive = false;
+						} else {
+							hit.collider.gameObject.GetComponent<Renderer> ().material.color = Color.red;
+							GuiManager.Instance.setGrid (key);
+							GuiManager.rotateButtonActive = true;
+						}
+					}
+					if (hit.collider.gameObject.name.StartsWith ("wall_")) {
+						string key = hit.collider.gameObject.name.Replace ("wall_", "");
+						curObject = GridController.walls[key];
+						prevGrid = key;
+					}
+					prevObject = hit.collider.gameObject;
 
+				}
 			}
 		}
 	}
--- Assets/Scripts/GuiManager.cs
+++ Assets/Scripts/GuiManager.cs
@@ -15,9 +15,11 @@ public class GuiManager : MonoBehaviour {
 	public Button GoldPouf;
 
 	private string grid = "";
-
+	private GameObject rotateObject = null;
+	public static bool rotateButtonActive = false;
 	public static GuiManager Instance;
 	void Awake(){
+		//rotateButton = GetComponent<Button> ();
 		Instance = this;
 		BlackToilet.onClick.AddListener(() => Placer("toilet"));
 		WhiteToilet.onClick.AddListener(() => Placer("toilet_2"));
@@ -27,11 +29,22 @@ public class GuiManager : MonoBehaviour {
 		BluePouf.onClick.AddListener(() => Placer("pouf_4"));
 
 	}
-
+	void OnGUI() // deprecated, use ordinary .UI now available in Unity
+	{
+		if (rotateButtonActive){
+			GUIStyle style = new GUIStyle ();
+			style.fontSize = 42;
+			style.fontStyle = FontStyle.Bold;
+			if (GUI.Button (new Rect (50, 50, 50, 50), "↺", style)) {
+				rotateObject = GridController.floor[grid];
+				rotateObject.transform.Rotate (new Vector3(0,0,90));
+				//rotateObject.transform.rotation.z = rotateObject.transform.rotation.z+90;
+			}
+		}
+	}
 	public void setGrid(string ng){
 		grid = ng;
 	}
-		
 
 	public void Placer(string name){
 		PropMenu.SetActive (true);
@@ -65,6 +78,7 @@ public class GuiManager : MonoBehaviour {
 			break;
 		}
 		PropMenu.SetActive (false);
+		rotateButtonActive = true;
 		FloorController.allowFloorClick = true;
 	}
 }
